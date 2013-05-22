b2d86dff1c97fe39a2d2537aef82cc982fc5bcc2
Severin Heiniger <severinheiniger@gmail.com>
2013-05-23 00:31:35 +0200

    Improve ContentSanitizer.

--- src/main/java/ch/ethz/nlp/headline/preprocessing/ContentSanitizer.java
+++ src/main/java/ch/ethz/nlp/headline/preprocessing/ContentSanitizer.java
@@ -5,7 +5,7 @@ public class ContentSanitizer implements ContentPreprocessor {
 	@Override
 	public String preprocess(String content) {
 		// Drop prefixes such as: BRUSSELS, Belgium (AP) -
-		content = content.replaceAll("\\w+, \\w+ \\(AP\\) . ", "");
+		content = content.replaceAll("\\w+, \\w+ \\(AP\\) (--|_|-) ", "");
 
 		// Normalize Q & A article with formatting
 		content = content.replaceAll("Q. \\(italics\\)", "");
