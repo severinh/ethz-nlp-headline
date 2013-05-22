5785870be971fe273e02f5de28e46d05649188fd
Raphael Fuchs <fuchsra@ethz.ch>
2013-05-23 00:27:45 +0200

    Minor sanitization and rewrite of 'says that'

--- src/main/java/ch/ethz/nlp/headline/preprocessing/AcronymCreator.java
+++ src/main/java/ch/ethz/nlp/headline/preprocessing/AcronymCreator.java
@@ -5,8 +5,10 @@ public class AcronymCreator implements ContentPreprocessor {
 	@Override
 	public String preprocess(String content) {
 		content = content.replaceAll("United States", "U.S.");
+		content = content.replaceAll("United Nations", "UN");
 		content = content.replaceAll("America Online", "AOL");
 		content = content.replaceAll("Corp.", "");
+		content = content.replaceAll("said that", "says");
 
 		return content;
 	}
