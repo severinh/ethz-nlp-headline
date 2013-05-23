1361d3c22009c1f2a7988f44008fbd4f674eaf87
Raphael Fuchs <fuchsra@ethz.ch>
2013-05-23 16:57:13 +0200

    Revert changeset e16ba5c

--- src/main/java/ch/ethz/nlp/headline/preprocessing/AcronymCreator.java
+++ src/main/java/ch/ethz/nlp/headline/preprocessing/AcronymCreator.java
@@ -5,10 +5,8 @@ public class AcronymCreator implements ContentPreprocessor {
 	@Override
 	public String preprocess(String content) {
 		content = content.replaceAll("United States", "U.S.");
-		content = content.replaceAll("United Nations", "UN");
 		content = content.replaceAll("America Online", "AOL");
 		content = content.replaceAll("Corp.", "");
-		content = content.replaceAll("said that", "says");
 
 		return content;
 	}
--- src/main/java/ch/ethz/nlp/headline/preprocessing/ContentSanitizer.java
+++ src/main/java/ch/ethz/nlp/headline/preprocessing/ContentSanitizer.java
@@ -7,9 +7,6 @@ public class ContentSanitizer implements ContentPreprocessor {
 		// Drop prefixes such as: BRUSSELS, Belgium (AP) -
 		content = content.replaceAll("\\w+, \\w+ \\(AP\\) (--|_|-) ", "");
 
-		// Replace all "_" by commas
-		content = content.replaceAll("_", ",");
-
 		// Normalize Q & A article with formatting
 		content = content.replaceAll("Q. \\(italics\\)", "");
 		content = content.replaceAll("\\(end italics\\) A.", "");
