1e454ffe1648f9ac814c6d22ba06dde74dc5957a
Severin Heiniger <severinheiniger@gmail.com>
2013-05-25 23:19:27 +0200

    Try to improve content sanitizing.

--- src/main/java/ch/ethz/nlp/headline/preprocessing/ContentSanitizer.java
+++ src/main/java/ch/ethz/nlp/headline/preprocessing/ContentSanitizer.java
@@ -4,16 +4,17 @@ public class ContentSanitizer implements ContentPreprocessor {
 
 	@Override
 	public String preprocess(String content) {
-		// Drop prefixes such as: BRUSSELS, Belgium (AP) - 
-		content = content.replaceAll(".+, .+ \\(AP|Xinhua\\) (--|_|-) ", "");
-		
-		// Drop prefixes such as WASHINGTON _ 
-		content = content.replaceAll("[A_Z]+ _ ", "");
-		
+		// Drop prefixes such as: BRUSSELS, Belgium (AP) -
+		content = content.replaceAll(".+ \\(AP|Xinhua\\) (--|_|-) ", "");
+
+		// Drop prefixes such as WASHINGTON _
+		content = content.replaceAll("([A-Z]| )+ _ ", "");
+
 		// Normalize Q & A article with formatting
 		content = content.replaceAll("Q. \\(italics\\)", "");
 		content = content.replaceAll("\\(end italics\\) A.", "");
 
 		return content;
 	}
+
 }
