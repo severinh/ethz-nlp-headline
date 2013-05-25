41b1d063027ededfd79cb4463dc362f92d1b6d0d
Severin Heiniger <severinheiniger@gmail.com>
2013-05-25 20:56:31 +0200

    Make it possible to compute the ROUGE-1 score for a single label.

--- src/main/java/ch/ethz/nlp/headline/util/RougeN.java
+++ src/main/java/ch/ethz/nlp/headline/util/RougeN.java
@@ -2,12 +2,14 @@ package ch.ethz.nlp.headline.util;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import com.google.common.collect.Sets;
 
 import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 
 /**
@@ -18,7 +20,8 @@ import edu.stanford.nlp.pipeline.Annotation;
  */
 public class RougeN {
 
-	private final List<Annotation> references;
+	private final List<Set<String>> referencesNGrams;
+	private final int denominator;
 	private final int n;
 
 	public RougeN(List<Annotation> references, int n) {
@@ -26,8 +29,16 @@ public class RougeN {
 
 		checkArgument(n > 0);
 
-		this.references = references;
+		this.referencesNGrams = new ArrayList<>(references.size());
 		this.n = n;
+
+		int denominator = 0;
+		for (Annotation reference : references) {
+			Set<String> referenceNGrams = CoreNLPUtil.getNGrams(reference, n);
+			referencesNGrams.add(referenceNGrams);
+			denominator += referenceNGrams.size();
+		}
+		this.denominator = denominator;
 	}
 
 	/**
@@ -41,15 +52,27 @@ public class RougeN {
 	 * @return the recall
 	 */
 	public double compute(Annotation candidate) {
-		double numerator = 0.0;
-		double denominator = 0.0;
+		int num = 0;
 		Set<String> candidateNGrams = CoreNLPUtil.getNGrams(candidate, n);
-		for (Annotation reference : references) {
-			Set<String> refNGrams = CoreNLPUtil.getNGrams(reference, n);
-			numerator += Sets.intersection(refNGrams, candidateNGrams).size();
-			denominator += refNGrams.size();
+		for (Set<String> referenceNGrams : referencesNGrams) {
+			num += Sets.intersection(referenceNGrams, candidateNGrams).size();
 		}
-		return numerator / denominator;
+		return (double) num / (double) denominator;
+	}
+
+	public double compute(CoreLabel candidateLabel) {
+		if (n > 1) {
+			return 0;
+		}
+
+		String candidateLemma = candidateLabel.lemma();
+		int num = 0;
+		for (Set<String> referenceNGram : referencesNGrams) {
+			if (referenceNGram.contains(candidateLemma)) {
+				num++;
+			}
+		}
+		return (double) num / (double) denominator;
 	}
 
 }
