24a9f93a56eaccfe0e23e4165383996dede3190f
Severin Heiniger <severinheiniger@gmail.com>
2013-05-26 10:57:16 +0200

    Speed up ContentSanitizer significantly.

--- src/main/java/ch/ethz/nlp/headline/preprocessing/ContentSanitizer.java
+++ src/main/java/ch/ethz/nlp/headline/preprocessing/ContentSanitizer.java
@@ -1,18 +1,25 @@
 package ch.ethz.nlp.headline.preprocessing;
 
+import java.util.regex.Pattern;
+
 public class ContentSanitizer implements ContentPreprocessor {
+
+	// Drop prefixes such as: BRUSSELS, Belgium (AP) -
+	private static final Pattern PREFIX_PATTERN_1 = Pattern
+			.compile(".+ \\((AP|Xinhua)\\) (--|_|-) ");
+
+	// Drop prefixes such as WASHINGTON _
+	private static final Pattern PREFIX_PATTERN_2 = Pattern
+			.compile("([A-Z]| )+(, \\w+)? _ ");
 
 	@Override
 	public String preprocess(String content) {
-		// Drop prefixes such as: BRUSSELS, Belgium (AP) -
-		content = content.replaceAll(".+ \\((AP|Xinhua)\\) (--|_|-) ", "");
-
-		// Drop prefixes such as WASHINGTON _
-		content = content.replaceAll("([A-Z]| )+(, \\w+)? _ ", "");
+		content = PREFIX_PATTERN_1.matcher(content).replaceFirst("");
+		content = PREFIX_PATTERN_2.matcher(content).replaceFirst("");
 
 		// Normalize Q & A article with formatting
-		content = content.replaceAll("Q. \\(italics\\)", "");
-		content = content.replaceAll("\\(end italics\\) A.", "");
+		content = content.replace("Q. (italics)", "");
+		content = content.replace("(end italics) A.", "");
 
 		return content;
 	}
